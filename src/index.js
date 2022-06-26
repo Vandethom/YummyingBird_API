2c7e1900c369824343e80f6ecef23439263fc450
Thomas Vandenbussche <thomas.vandenbussche@outlook.com>
2022-06-26 20:17:27 +0200

    heroku config test

--- src/index.js
+++ src/index.js
@@ -33,6 +33,13 @@ const limiter = rateLimit({
 	max: 100                     // 100 requests per IP
 })
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", '*');
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	next();
+  })
 
 app.use(
 	cors(),
